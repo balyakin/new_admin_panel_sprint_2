9731af9fdc351e4bb8992d20d3cbcef993183aab
Евгений Балякин <balyakin@algont.ru>
2022-03-18 17:07:14 +0300

    added nginx container

--- 01_doker_compose/app/entrypoint.sh
+++ 01_doker_compose/app/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-export DJANGO_SUPERUSER_PASSWORD=admin
-# wait postgres to start
-sleep 20
+while ! nc -z $DB_HOST 5432; do sleep 1; done;
 cd /app
 python manage.py migrate
 python manage.py createsuperuser --noinput --username admin --email test@test.com
---
+++ 01_doker_compose/imports/init.sql
@@ -0,0 +1,4 @@
+\c movies_database
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE SCHEMA IF NOT EXISTS content;
